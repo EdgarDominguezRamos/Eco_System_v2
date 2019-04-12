752d685fd3d5b8cd36fd80ed101cf6e1faefcd71
EdgarDominguezRamos <1717110264@utectulancingo.edu.mx>
2019-04-12 15:18:33 -0500

    interfaces corregidas

--- app/src/main/java/com/example/ecosystem_v2/Activity_Perfil.java
+++ app/src/main/java/com/example/ecosystem_v2/Activity_Perfil.java
@@ -29,10 +29,6 @@ public class Activity_Perfil extends AppCompatActivity {
 
     private ListView lv_post;
     private ArrayAdapter adapter;
-
-    TextView tv_nombre_usuario;
-    EditText et_descripcion;
-
     private String url = "https://webserviceedgar.herokuapp.com/api_post?user_hash=12345&action=get&id_usurio_eco=1";
 
     @Override
@@ -42,22 +38,11 @@ public class Activity_Perfil extends AppCompatActivity {
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
 
-        lv_post = findViewById(R.id.lv_post);
-        adapter = new ArrayAdapter(this, R.layout.activity__new__post);
+        lv_post = (ListView)findViewById(R.id.lv_post);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
         lv_post.setAdapter(adapter);
 
         webServiceRest(url);
-
-        tv_nombre_usuario = findViewById(R.id.tv_nombre_usuario);
-        et_descripcion = findViewById(R.id.et_descripcion);
-        //Objeto tipo Intent para recuperar el parametro enviado
-        Intent intent = getIntent();
-        //Se almacena el id_cliente enviado
-        //String id_usuario_eco = intent.getStringExtra(Activity_Perfil.ID_USUARIO_ECO);
-        //Se cocnatena la url con el id_cliente para obtener los datos el cliente
-        //url+=id_usuario_eco;
-        //webServiceRest(url);
-        
     }
 
     private void webServiceRest(String requestURL){
@@ -73,7 +58,7 @@ public class Activity_Perfil extends AppCompatActivity {
             bufferedReader.close();
             parseInformation(webServiceResult);
         }catch(Exception e){
-            Log.e("Error 100",e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -81,37 +66,23 @@ public class Activity_Perfil extends AppCompatActivity {
         JSONArray jsonArray = null;
         String id_post;
         String titulo;
-        String procedimiento;
-        String link_video;
-        String imagen;
+
         try{
             jsonArray = new JSONArray(jsonResult);
         }catch (JSONException e){
-            Log.e("Error 101",e.getMessage());
+            e.printStackTrace();
         }
         for(int i=0;i<jsonArray.length();i++){
             try{
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 id_post = jsonObject.getString("id_post");
                 titulo = jsonObject.getString("titulo");
-                procedimiento = jsonObject.getString("procedimiento");
-                link_video = jsonObject.getString("link_video");
-                imagen = jsonObject.getString("imagen");
-                adapter.add(titulo );
 
-                tv_nombre_usuario.setText((CharSequence) tv_nombre_usuario);
-                et_descripcion.setText((CharSequence) et_descripcion);
-                URL newurl = new URL(url+imagen);
-                Bitmap mIcon_val = BitmapFactory.decodeStream(newurl.openConnection() .getInputStream());
-                //iv_imagen.setImageBitmap(mIcon_val);
-
+                adapter.add(id_post + ": " + titulo);
             }catch (JSONException e){
-                Log.e("Error 102",e.getMessage());
-            } catch (MalformedURLException e) {
-                Log.e("Error 103",e.getMessage());
-            } catch (IOException e) {
-                Log.e("Error 104",e.getMessage());
+                e.printStackTrace();
             }
         }
     }
+
 }
