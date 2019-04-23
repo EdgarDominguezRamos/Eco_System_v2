98d5e0524115766ca944855a05fac6bc0a4c9363
Abigailnm <1717110627@utectulancingo.edu.mx>
2019-04-22 18:08:39 -0600

    activity comentarios

---
+++ app/src/main/java/com/example/ecosystem_v2/Activity_Comentarios.java
@@ -0,0 +1,13 @@
+package com.example.ecosystem_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Comentarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__comentarios);
+    }
+}
